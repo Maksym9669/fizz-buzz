de9ae0ea1fab1b5b6d75319ee5c8e072b95ee9ac
Maksym9669 <maksym.sychov14@gmail.com>
2019-02-25 22:10:46 -0500

    Updates

---
+++ precache-manifest.ec3a25d4d96da32c221338fbd4b247f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1a459156dea332fd4fb",
+    "url": "/fizz-buzz/static/css/main.ae8f9007.chunk.css"
+  },
+  {
+    "revision": "c1a459156dea332fd4fb",
+    "url": "/fizz-buzz/static/js/main.c1a45915.chunk.js"
+  },
+  {
+    "revision": "ca065b4dab31c1550549",
+    "url": "/fizz-buzz/static/js/runtime~main.ca065b4d.js"
+  },
+  {
+    "revision": "9482b37333f464dcf387",
+    "url": "/fizz-buzz/static/js/2.9482b373.chunk.js"
+  },
+  {
+    "revision": "495c6ec7361a2996b567c546c068e5fa",
+    "url": "/fizz-buzz/index.html"
+  }
+];
